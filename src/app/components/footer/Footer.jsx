3af9a1c12c384efc331d86e9260af33524666fcb
datodato788 <gogiawenom8@gmail.com>
2023-12-28 20:08:51 +0400

    navbar and page.jsx edit

--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import styles from "./footer.module.css"
+import React from 'react';
+import styles from '../footer/footer.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
+
 
 function Footer() {
   return (
-    <div className={styles.container}>MADE BY დათო გოგია</div>
-  )
-}
+    <div className={styles.container}>
+      <div>MADE BY დათო გოგია</div>
+      <div className={styles.socialNetworks}>
 
-export default Footer
+        <a href='https://www.facebook.com/'>
+          <Image src="/fb.png" width={15} className={styles.Icon} height={15} alt='Facebook' />
+        </a>
+
+        <a href='https://www.instagram.com/'>
+          <Image src="/ig.png" width={15} className={styles.Icon} height={15} alt='Instagram' />
+        </a>
+
+        <a href='https://twitter.com/'>
+          <Image src="/x.png" width={15} className={styles.Icon} height={15} alt='Twitter' />
+        </a>
+        <a href='https://www.youtube.com/'>
+          <Image src="/yt.png" width={15} className={styles.Icon} height={15} alt='Youtube' />
+        </a>
+
+
+
+      </div>
+    </div>
+  );
+}
+export default Footer;
