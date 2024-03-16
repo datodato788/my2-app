9f033b742032ad08e66e88911405bfe25da912ff
datodato788 <151830320+datodato788@users.noreply.github.com>
2024-03-16 13:22:37 +0400

    sport_update

--- src/app/FOOTball/page.jsx
+++ src/app/FOOTball/page.jsx
@@ -12,43 +12,43 @@ function FOOTball() {
   return (
     <div className={styles.container}>
     <div>
-      <Image
+    <Link href={"/FOOTball/N"}> <Image
         src={NNN1}
         alt='NNN1'
         className={styles.top} />
       <p className={styles.p}>N1</p>
-      <Link href={"/FOOTball/N"}><p>Cristiano Ronaldo</p></Link>
+     <p>Cristiano Ronaldo</p></Link>
     </div>
     <div>
-      <Image
+    <Link href={"/FOOTball/NN"}> <Image
         src={NNN2}
         alt='NNN2'
         className={styles.top} />
       <p className={styles.p}>N2</p>
-      <Link href={"/FOOTball/NN"}><p className={styles.fix}>Lionel Messi</p></Link>
+     <p className={styles.fix}>Lionel Messi</p></Link>
     </div>
     <div>
-      <Image
+    <Link href={"/FOOTball/NNN"}><Image
         src={NNN3}
         alt='NNN3'
         className={styles.top} />
       <p className={styles.p}>N3</p>
-      <Link href={"/FOOTball/NNN"}><p>Karim Benzema</p></Link>
+      <p>Karim Benzema</p></Link>
     </div>
     <div>
-      <Image
+    <Link href={"/FOOTball/NNNN"}> <Image
         src={NNN4}
         alt='NNN4'
         className={styles.top} />
       <p className={styles.p}>N4</p>
-      <Link href={"/FOOTball/NNNN"}><p>Neymar</p></Link>
+     <p>Neymar</p></Link>
     </div><div>
-      <Image
+    <Link href={"/FOOTball/NNNNN"}> <Image
         src={NNN5}
         alt='NNN5'
         className={styles.top} />
       <p className={styles.p}>N5</p>
-      <Link href={"/FOOTball/NNNNN"}><p>Khvicha Kvaratskhelia</p></Link>
+      <p>Khvicha Kvaratskhelia</p></Link>
     </div>   
   </div>
   )
--- src/app/MMA/page.jsx
+++ src/app/MMA/page.jsx
@@ -13,43 +13,43 @@ function MMA() {
       
     <div className={styles.container}>
     <div>
-      <Image
+    <Link href={"/MMA/W"}> <Image
         src={NN1}
         alt='NN1'
         className={styles.top} />
       <p className={styles.p}>N1</p>
-      <Link href={"/MMA/W"}><p>Khabib Nurmagomedov</p></Link>
+   <p>Khabib Nurmagomedov</p></Link>
     </div>
     <div>
-      <Image
+    <Link href={"/MMA/WW"}> <Image
         src={NN2}
         alt='NN2'
         className={styles.top} />
       <p className={styles.p}>N2</p>
-      <Link href={"/MMA/WW"}><p className={styles.fix}>Georges Saint-Pierre</p></Link>
+     <p className={styles.fix}>Georges Saint-Pierre</p></Link>
     </div>
     <div>
-      <Image
+    <Link href={"/MMA/WWW"}> <Image
         src={NN3}
         alt='NN3'
         className={styles.top} />
       <p className={styles.p}>N3</p>
-      <Link href={"/MMA/WWW"}><p>Jon Jones</p></Link>
+     <p>Jon Jones</p></Link>
     </div>
     <div>
-      <Image
+    <Link href={"/MMA/WWWW"}><Image
         src={NN4}
         alt='NN4'
         className={styles.top} />
       <p className={styles.p}>N4</p>
-      <Link href={"/MMA/WWWW"}><p>Anderson Silva</p></Link>
+      <p>Anderson Silva</p></Link>
     </div><div>
-      <Image
+    <Link href={"/MMA/WWWWW"}> <Image
         src={NN5}
         alt='NN5'
         className={styles.top} />
       <p className={styles.p}>N5</p>
-      <Link href={"/MMA/WWWWW"}><p>Fedor Emelianenko</p></Link>
+   <p>Fedor Emelianenko</p></Link>
     </div>   
   </div>
 
